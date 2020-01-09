1897d87211569efee82a6f72caf23465df995ac4
leo0530 <276549252@qq.com>
2020-01-09 10:12:59 +0800

    添加打磨状态并测试,修改作业状态、系统参数界面,修改检修调试界面

--- TrainMilling2/TrainMilling/Debug/BAT00008D19323692.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-copy  "c:\Documents and Settings\Administrator\My Documents\Visual Studio 2008\Projects\TrainMilling\TrainMilling\systemconfiguration.ini" "c:\Documents and Settings\Administrator\My Documents\Visual Studio 2008\Projects\TrainMilling\TrainMilling\Debug\Output\appl\..\cfg\systemconfiguration.ini"
-
-if errorlevel 1 goto VCReportError
-goto VCEnd
-:VCReportError
-echo Project : error PRJ0019: A tool returned an error code from "Copy  "c:\Documents and Settings\Administrator\My Documents\Visual Studio 2008\Projects\TrainMilling\TrainMilling\systemconfiguration.ini" "c:\Documents and Settings\Administrator\My Documents\Visual Studio 2008\Projects\TrainMilling\TrainMilling\Debug\Output\appl\..\cfg\systemconfiguration.ini""
-exit 1
-:VCEnd
